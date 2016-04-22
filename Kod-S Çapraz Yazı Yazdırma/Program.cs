488654d3359918ac0b0382bff465b996d74eb34b
Murat Bilginer <mrtblgnr@muratbilginer.net>
2016-04-22 18:48:06 +0300

    1 Kod-S Çapraz Yazı Yazdırma v1.0 Gloster
    Çapraz Yazı Yazdırma ilk versiyonu

---
+++ Kod-S Çapraz Yazı Yazdırma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Çapraz_Yazı_Yazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Industryolog";
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine("{0," + (i + s.Length-1) + "}", s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
